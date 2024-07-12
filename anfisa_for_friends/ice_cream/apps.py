635c7a181b174805757e3d275a57c2a297f584a2
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-07-12 18:30:10 +0500

    learining admin zone

--- anfisa_for_friends/ice_cream/apps.py
+++ anfisa_for_friends/ice_cream/apps.py
@@ -4,3 +4,22 @@ from django.apps import AppConfig
 class IceCreamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ice_cream'
+    verbose_name = 'Каталог мороженого'
+
+
+class ToppingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'topping'
+    verbose_name = 'Топпинг'
+
+
+class WrapperConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wrapper'
+    verbose_name = 'Обёртка'
+
+
+class IceCreamAdminConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ice_cream_admin'
+    verbose_name = 'Мороженое'
