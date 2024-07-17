bd325d0ff097a416dc6361bf2bbba429ab1d1a2c
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-07-17 13:19:54 +0500

    New templates, fixed homepage/views and ice_cream/views. fixed ice_cream/models.

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,10 +1,17 @@
+from django.db.models import Q
 from django.shortcuts import render
 from ice_cream.models import IceCream
 
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'price', 'description'
+    ).filter(
+        is_published=True,
+        is_on_main=True,
+        category__is_published=True
+    )
     context = {
         'ice_cream_list': ice_cream_list,
     }
